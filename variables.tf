23d2eeb56ff253d6a8cf2d93775d023f87db7212
Karan Anpat <121686830+karananpat10@users.noreply.github.com>
2025-08-07 16:40:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,20 +5,21 @@ variable "region" {
 
 # Declare the bucket name
 variable "bucket_name_prefix" {
-  default = "finalyelp2012310"
+  default = "finalyelp2012312"
 }
 
 # Declare the Glue job name
 variable "glue_job_name" {
-  default = "yelpdata1"
+  default = "yelpdata2"
 }
 
 # Declare the Glue crawler name
 variable "glue_crawler_name" {
-  default = "yelpcrawlers12"
+  default = "yelpcrawlers13"
 }
 
 # Declare the script path in S3
 variable "script_s3_path" {
-  default = "s3://finalyelp2012310/scripts/glue_job.py"
+  default = "s3://finalyelp2012312/scripts/glue_job.py"
+
 }
